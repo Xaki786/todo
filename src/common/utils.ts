b1f01c071aaba31008fe7995d3f7fb768bae9df4
zakria jan <zakria.jan@carbonteq.com>
2023-01-03 14:03:40 +0500

    added some cool stuff in logger

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -3,10 +3,19 @@
 export function getEnvironment() {
   return process.env.NODE_ENV || "development";
 }
-export const appDevelopmentLogger = (data: unknown) => {
+type loggerOptions = {
+  context?: string;
+};
+export const appDevelopmentLogger = (
+  data: unknown,
+  options?: loggerOptions
+) => {
+  const context = options?.context ?? "No Context Provided";
   const env = getEnvironment();
   if (env !== "development") return;
-  console.log("=======================");
+  console.log("=================================================");
+  console.log(`               Context: ${context}               `);
+  console.log("=================================================");
   console.log(data);
-  console.log("=======================");
+  console.log("=================================================");
 };
