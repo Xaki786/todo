91d55674c33fc64801576d9d2bd6b8adc691d029
zakria jan <zakria.jan@carbonteq.com>
2023-01-31 22:51:53 +0500

    removed remainings of auth service

--- src/application/services/index.ts
+++ src/application/services/index.ts
@@ -2,7 +2,6 @@
 
 export * from "./tasks.service";
 export * from "./users.service";
-export * from "./auth.service";
 export * from "./CreateUserService";
 export * from "./RegisterUserService";
 export * from "./LoginUserService";
